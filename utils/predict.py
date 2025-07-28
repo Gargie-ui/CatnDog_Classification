68cf507512e1abac0fd3b8dbf375cafe0882513b
Gargi Channe <gargichanne@gmail.com>
2025-07-28 23:28:08 +0530

    Update predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -3,8 +3,14 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-# Load the model
-model = load_model("model/cat_dog_neither_classifier_mobilenet.h5")
+from tensorflow.keras.models import load_model
+
+# Load your old model
+model = load_model("model/cat_dog_neither_classifier_mobilenet.h5", compile=False)
+
+# Re-save it in a cleaner .h5 format
+model.save("model/cat_dog_neither_classifier_cleaned.h5", save_format='h5')
+
 
 # Class names — must match the order used during training
 class_names = ['cat', 'dog', 'neither']
